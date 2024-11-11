0127f61083d5bf379d896270ca10b9302d379fe7
LeoGee1 <ucheleonard2000@gmail.com>
2024-11-11 10:40:41 +0100

    minor changes to the about-page and home-page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,36 +1,35 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+
 import { useRef, useState } from 'react';
 
 const About = () => {
     const swapref = useRef(null);
     const [flip, setFlip] = useState(null);
 
-    const handleClick = () => {
-        setFlip(true);
+    // const handleClick = () => {
+    //     setFlip(true);
 
-        if(flip) {
-            const potraits = document.querySelector('.potraits');
-            const description = document.querySelector('.description');
-            const swap = swapref.current;
+    //     if(flip) {
+    //         const potraits = document.querySelector('.potraits');
+    //         const description = document.querySelector('.description');
+    //         const swap = swapref.current;
 
-            potraits.classList.add('order-third');
-            swap.classList.add('order-second');
-            description.classList.add('order-first');
+    //         potraits.classList.add('order-third');
+    //         swap.classList.add('order-second');
+    //         description.classList.add('order-first');
 
-            setFlip(false);
-        }
+    //         setFlip(false);
+    //     }
 
-        if(!flip) {
-            const potraits = document.querySelector('.potraits');
-            const description = document.querySelector('.description');
-            const swap = swapref.current;
+    //     if(!flip) {
+    //         const potraits = document.querySelector('.potraits');
+    //         const description = document.querySelector('.description');
+    //         const swap = swapref.current;
 
-            potraits.classList.remove('order-third');
-            swap.classList.remove('order-second');
-            description.classList.remove('order-first');
-        }       
-    }
+    //         potraits.classList.remove('order-third');
+    //         swap.classList.remove('order-second');
+    //         description.classList.remove('order-first');
+    //     }       
+    // }
    
     return ( 
         <div className="aboutme">
@@ -42,16 +41,11 @@ const About = () => {
                 <div className="boxes five"></div>
                 <div className="boxes six"></div>
             </div>
-            {/* <div className="line" ref={swapref} onClick={handleClick}>
-                 <FontAwesomeIcon className='dots' icon={faEllipsisVertical} />
-            </div> */}
             <div className="description">
-                {/* <h2 className='first-description'>A Frontend Developer with the right attitude for Software Development. equipped with</h2>
-                <h2 className='second-description'>for the works of a man showeth his ability.for the works of a man showeth his ability.</h2>
-                <h2 className='third-description'>for the works of a man showeth his ability.for the works of a man showeth his ability.</h2>
-                <h2 className='fourth-description'>for the works of a man showeth his ability.for the works of a man showeth his ability.</h2> <h2 className='fifth-description'>for the works of a man showeth his ability.for the works of a man showeth his ability.</h2>
-                <h2 className='sixth-description'>for the works of a man showeth his ability.for the works of a man showeth his ability.</h2> */}
-                <h2 className='description-child'>What do you wanna know about this individual? Apart from the fact I am a self taught developer with professional guidance as you may have seen on HOME. I also posses hands-on experince and technical know how on how to turn your designs into a full working webflow with my set of skills which keep on improving daily. looking forward to take on more tasks that would put me on my feet and get better at my craft</h2></div>
+                <h2 className='description-child'>
+                    Curious about me? I'm a self-taught front-end developer who’s had the good fortune of some professional mentorship. With hands-on experience, I specialize in bringing designs to life in fully interactive, smooth webflows. My skills grow daily! think of me as the software update you actually look forward to! I’m always up for tasks that keep me on my toes and push me to get even better at my craft.
+                </h2>
+            </div>
         </div>
      );
 }
